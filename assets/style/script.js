0adad4b906aabb789ee15ee8edd4212651f25776
Syarif09 <syarif.khalid@gmail.com>
2024-06-20 17:00:44 +0700

    checkpoint

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,11 +1,17 @@
 const menuBtn = document.getElementById('menu')
 const nav = document.getElementById('nav')
 const navCenter = nav.getElementsByClassName('navbar-opt').item(0)
+const navRight = nav.getElementsByClassName('navbar-button').item(0)
 
 menuBtn.onclick = () => {
     if (navCenter.getAttribute('style')) {
         navCenter.removeAttribute('style')
     } else {
         navCenter.setAttribute('style', 'display:none')
+    }
+    if (navRight.getAttribute('style')) {
+        navRight.removeAttribute('style')
+    } else {
+        navRight.setAttribute('style', 'display:none')
     }
 }
