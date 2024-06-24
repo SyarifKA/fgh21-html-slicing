282cf8a5c4cf69aa379a27e87dce2c21217a3b08
Syarif09 <syarif.khalid@gmail.com>
2024-06-24 14:53:15 +0700

    DOM Implementation : Sign page

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,3 +1,19 @@
+// Login
+function processData(event) {
+    event.preventDefault()
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const password = event.target.password.value
+    if(username === 'syarif' && email === 'syarif@gmail.com' && password === '1234'){
+        alert('Login Success!')
+    }else if(username === '' && email === '' && password === ''){
+        alert('Masukkan akun Anda untuk Login')
+    }else{
+        alert('Data yang Anda masukkan salah!')
+    }
+}
+
+// Navbar
 const menuBtn = document.getElementById('menu')
 const nav = document.getElementById('nav')
 const navCenter = nav.getElementsByClassName('navbar-opt').item(0)
