b094a8ec4906a1964f35a14c86c45e46cdfc46b2
Syarif09 <syarif.khalid@gmail.com>
2024-06-26 16:53:28 +0700

    integration sign page and home page

--- assets/style/script.js
+++ assets/style/script.js
@@ -5,11 +5,17 @@ function processData(event) {
     const email = event.target.email.value
     const password = event.target.password.value
     if(username === 'syarif' && email === 'syarif@gmail.com' && password === '1234'){
-        alert('Login Success!')
+        window.location = 'index.html'
     }else if(username === '' && email === '' && password === ''){
         alert('Enter your account to Login')
     }else{
-        alert('The data you entered is incorrect!')
+        const alertElement = document.getElementById('alert')
+        alertElement.setAttribute('style', 'padding: 10px; border-radius:10px; color: red; background-color: salmon;')
+
+        const text = document.createElement('span')
+
+        text.textContent = 'Wrong Email or Password'
+        alertElement.appendChild(text)
     }
 }
 
@@ -36,15 +42,6 @@ menuBtn.onclick = () => {
 function appendElement(elementId, contentChild) {
     const container = document.getElementById(elementId)
 
-    // for (let i = 0; i < arr.length; i++){
-    //     const content = document.createElement('span')
-    //     content.textContent = arr[i]
-    //     if (arr[i] == arr[0]) {
-    //         content.classList.add('header')
-    //     }
-    //     container.appendChild(content)
-    // }
-
     contentChild.forEach(item => {
         const content = document.createElement('span')
         content.textContent = item
@@ -69,48 +66,96 @@ footertext.forEach((item, index) => {
 
 
 // Card
+
 const contentFilm = document.getElementById('content-film')
 
-const eventFilm = document.createElement('div')
-eventFilm.classList.add('film')
+const events = [
+    {
+        title: 'Sights & Sounds Exhibition',
+        date: 'Wed, 15 Nov, 4:00 PM',
+        image: './assets/image/event-1.png'
+    },
+    {
+        title: 'See it in Gold Class',
+        date: 'Thu, 16 Nov, 7:00 PM',
+        image: './assets/image/event-2.png'
+    },
+    {
+        title: 'music concert',
+        date: 'Sat, 16 Jun, 7:00 PM',
+        image: './assets/image/event-3.jpg'
+    },
+    {
+        title: 'MotoGP at Mandalika',
+        date: 'Sun, 27 Aug, 10:00 AM',
+        image: './assets/image/event-4.jpg'
+    },
+    {
+        title: 'international chess tournament',
+        date: 'Sun, 7 Feb, 08:00 AM',
+        image: './assets/image/event-5.jpg'
+    },
+    {
+        title: 'Jakarta International Marathon',
+        date: 'Sun, 23 Jun, 03:00 AM',
+        image: './assets/image/event-6.jpg'
+    },
+    {
+        title: 'Indonesia VS Argentina',
+        date: 'Wed, 21 Jul, 18:00 PM',
+        image: './assets/image/event-7.jpg'
+    }
+]
 
-const spanShadow = document.createElement('span')
-spanShadow.classList.add('shadow')
+events.forEach(event => {
+    const eventFilm = document.createElement('div')
+    eventFilm.classList.add('film')
 
-const dayEvent = document.createElement('div')
-dayEvent.classList.add('day')
-dayEvent.textContent = 'Wed, 15 Nov, 4:00 PM'
+    const picEvent = document.createElement('img')
+    picEvent.setAttribute('src', event.image)
+    picEvent.setAttribute('alt', 'Event-1')
 
-const titleEvent = document.createElement('div')
-titleEvent.classList.add('title')
-titleEvent.textContent = 'Sights & Sounds Exhibition'
+    const spanShadow = document.createElement('span')
+    spanShadow.classList.add('shadow')
 
-const peopleEvent = document.createElement('div')
-peopleEvent.classList.add('people')
+    const dayEvent = document.createElement('div')
+    dayEvent.classList.add('day')
+    dayEvent.textContent = event.date
 
-const people1 = document.createElement('div')
-people1.classList.add('people1')
+    const titleEvent = document.createElement('div')
+    titleEvent.classList.add('title')
+    titleEvent.textContent = event.title
 
-const people2 = document.createElement('div')
-people2.classList.add('people2')
+    const peopleEvent = document.createElement('div')
+    peopleEvent.classList.add('people')
 
-const people3 = document.createElement('div')
-people3.classList.add('people3')
+    const people1 = document.createElement('div')
+    people1.classList.add('people1')
 
-const people4 = document.createElement('div')
-people4.classList.add('people4')
+    const people2 = document.createElement('div')
+    people2.classList.add('people2')
 
-const spanLayer = document.createElement('span')
-spanLayer.classList.add('layer')
-spanLayer.textContent = '+62'
+    const people3 = document.createElement('div')
+    people3.classList.add('people3')
 
-people4.appendChild(spanLayer)
-peopleEvent.appendChild(people1)
-peopleEvent.appendChild(people2)
-peopleEvent.appendChild(people3)
-peopleEvent.appendChild(people4)
-spanShadow.appendChild(dayEvent)
-spanShadow.appendChild(titleEvent)
-spanShadow.appendChild(peopleEvent)
-eventFilm.appendChild(spanShadow)
-contentFilm.appendChild(eventFilm)
+    const people4 = document.createElement('div')
+    people4.classList.add('people4')
+
+    const spanLayer = document.createElement('span')
+    spanLayer.classList.add('layer')
+    spanLayer.textContent = '+62'
+
+    people4.appendChild(spanLayer)
+    peopleEvent.appendChild(people1)
+    peopleEvent.appendChild(people2)
+    peopleEvent.appendChild(people3)
+    peopleEvent.appendChild(people4)
+    spanShadow.appendChild(dayEvent)
+    spanShadow.appendChild(titleEvent)
+    spanShadow.appendChild(peopleEvent)
+    eventFilm.appendChild(picEvent)
+    eventFilm.appendChild(spanShadow)
+    contentFilm.appendChild(eventFilm)
+    
+})
+
